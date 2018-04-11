e2c3b3bf3b3fb184fe46178c347bc66300754d2f
Sergio Aguilar <sergeagui@gmail.com>
2018-04-10 20:42:42 -0500

    first hello

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace HelloWorld
-{
-    class Program
+namespace HelloWorld{
+					
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello, Sergio!");
+            int prin = 0;
+            decimal rate = 0.0M;
+            int time = 0;
+            
+            Console.WriteLine("Please enter the Principal amount: ");
+            prin = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the Intrest Rate: ");
+            rate = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Please enter the time period in years: ");
+            time = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Your Accrued amount will be {0}", prin*(1+rate*time));
         }
     }
 }
