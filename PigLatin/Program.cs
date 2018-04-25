7d6309f7448c2504745cf310b22fd74b6706faa8
Sergio Aguilar <sergeagui@gmail.com>
2018-04-24 20:56:31 -0500

    saving

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -8,15 +8,10 @@ namespace PigLatin
         public static void Main()
         {
 
-            Console.WriteLine("enter a word");
+            Console.WriteLine("enter a sentance");
             string input = Console.ReadLine();
 
-            if (input.Any(Char.IsWhiteSpace))
-            {
-                Console.Write(TranslateSentance(input));
-            }
-            
-            Console.Write(TranslateWord(input));
+            Console.WriteLine(TranslateSentance(input));
             
 
         }
@@ -24,13 +19,12 @@ namespace PigLatin
         public static string TranslateWord(string word)
         {
             
-            int firstVowelIndex = word.IndexOfAny(new char[] {'a', 'e', 'i', 'o', 'u'});
-
+            int firstVowelIndex = word.IndexOfAny(new char[] {'a', 'e', 'i', 'o', 'u', 'y'});
             string firstPart = word.Substring(0,firstVowelIndex);
             string secondPart = word.Substring(firstVowelIndex);
             
             //if index == zero, then word begins with a vowel... so just add 'yay' to the end of the word
-            if (firstVowelIndex == 0)
+            if (firstVowelIndex <= 0)
             {
                 return word + "yay";
             }
@@ -41,17 +35,22 @@ namespace PigLatin
 
         public static string TranslateSentance (string sentance)
         {
-            string[] sentanceWords = sentance.Split(' ');
-            string[] newSentance = new string[5];
-            for (int i = 0; i < sentanceWords.Length; i++)
-            {
-                newSentance[i] = sentanceWords[i].ToUpper();
-                TranslateWord(newSentance[5]);
-            }
-            string upperCaseString = String.Join(" ", newSentance);
-            Console.WriteLine(upperCaseString);
+            string[] words = sentance.Split(' ');
+            string[] translatedWords = new string[words.Length];
 
-            return sentance.ToUpper();
+            for (int i = 0; i < words.Length; i++)
+            {
+                translatedWords[i] = TranslateWord(words[i]);
+            }
+            return String.Join(' ', translatedWords);
+        }
+
+        private static string TranslatePunctuation (string punctuation)
+        {
+            int punctuationIndex = punctuation.IndexOfAny(new char[] { '.', ',', '!', '?' });
+            //still working
+            return "";
+
         }
     }
 }
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -4,16 +4,25 @@ namespace RockPaperScissors
 {
     class Program
     {
+        public static int hand1Score = 0;
+        public static int hand2Score = 0;
         public static void Main()
         {
-            Console.WriteLine("Enter hand 1:");
-            string hand1 = Console.ReadLine().ToLower();
-            Console.WriteLine("Enter hand 2:");
-            string hand2 = Console.ReadLine().ToLower();
-            Console.WriteLine(CompareHands(hand1, hand2));
-
-            // leave this command at the end so your program does not close automatically
-            Console.ReadLine();
+            string[] plays = new [] {"rock", "paper", "scissors"};
+            while (hand1Score < 5 && hand2Score < 5)
+            {
+                Console.WriteLine("Enter hand 1:");
+                string hand1 = Console.ReadLine().ToLower();
+                // Commented out to have a computer player 2
+                // Console.WriteLine("Enter hand 2:");
+                // string hand2 = Console.ReadLine().ToLower();
+                Random rnd = new Random();
+                int randomInt = rnd.Next(0, 3);
+                string hand2 = plays[randomInt];
+                Console.WriteLine("Hand 1 played {0} and Hand 2 played {1}", hand1, hand2);
+                Console.WriteLine(CompareHands(hand1, hand2));
+                Console.WriteLine("Hand 1 has {0} points and Hand 2 has {1} points", hand1Score, hand2Score);
+            }
         }
         
         public static string CompareHands(string hand1, string hand2)
@@ -27,10 +36,12 @@ namespace RockPaperScissors
             {
                 if (hand2 == "scissors")
                 {
+                    hand1Score++;
                     return "Hand one wins!";
                 }
                 if (hand2 == "paper")
                 {
+                    hand2Score++;
                     return "Hand two wins!";
                 }
             }
@@ -39,10 +50,12 @@ namespace RockPaperScissors
             {
                 if (hand2 == "scissors")
                 {
+                    hand2Score++;
                     return "Hand two wins";
                 }
                 if (hand2 == "rock")
                 {
+                    hand1Score++;
                     return "Hand one wins";
                 }
             }
@@ -50,10 +63,12 @@ namespace RockPaperScissors
             {
                 if (hand2 == "paper")
                 {
+                    hand1Score++;
                     return "Hand one wins";
                 }
                 if (hand2 == "rock")
                 {
+                    hand2Score++;
                     return "Hand two wins";
                 }
             }
