ec01bef534fffd54e250485e7db8783533a73f54
Sergio Aguilar <sergeagui@gmail.com>
2018-05-06 13:28:19 -0500

    saving progress

---
+++ OOP/OOP.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create a class car and garage
+            //make constructor (Main)
+            //follow spots in garage
+            //keep track of persons in cars
+            // get{
+            //     for (int i; i < cars.Length; i++)
+            //     {
+            //         if (cars[i] != null )
+            //         {
+            //             Console.WriteLine($"The {}")
+            //         }
+            //     }
+            // }
+        }
+    }
+    // class Car
+    // {
+        // public static int[] redCar;
+        // public static int[] greenCar;
+    //     public Car(int cars)
+    //     {
+    //     }
+    // }
+    class Garage
+    {
+        private static int[] redCar = {1};
+        private static int[] greenCar = {1};
+        public Garage(int[] space)
+        {
+            int[] spots = {3};
+        }
+        public int Car
+        {
+        }
+    }
+    class Persons
+    {
+        private string person1 = "Mike";
+        private string person2 = "Sam";
+        private string person3 = "Sally";
+        private string person4 = "Terry";
+        public int PersonProp { get; set; }
+    }
+}
