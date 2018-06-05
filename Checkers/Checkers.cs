9e6a9ed772eb44d2cfffd01546be8fa582d0a4de
Sergio Aguilar <sergeagui@gmail.com>
2018-06-04 20:39:09 -0500

    one more part of Checkers

--- Checkers/Checkers.cs
+++ Checkers/Checkers.cs
@@ -8,43 +8,49 @@ namespace Checkers
     {
         static void Main(string[] args)
         {
-            int[][] whitePositions = new int[][] {
-                new int[] { 0, 1 }, new int[] { 0, 3 }, new int[] { 0, 5 }, new int[] { 0, 7 },
-                new int[] { 1, 0 }, new int[] { 1, 2 }, new int[] { 1, 4 }, new int[] { 1, 6 },
-                new int[] { 2, 1 }, new int[] { 2, 3 }, new int[] { 2, 5 }, new int[] { 2, 7 }
-            };
-            
-
-            int[][] blackPositions = new int[][] {
-                new int[] { 5, 0 }, new int[] { 5, 2 }, new int[] { 5, 4 }, new int[] { 5, 6 },
-                new int[] { 6, 1 }, new int[] { 6, 3 }, new int[] { 6, 5 }, new int[] { 6, 7 },
-                new int[] { 7, 0 }, new int[] { 7, 2 }, new int[] { 7, 4 }, new int[] { 7, 6 }
-            };
-
             Board board = new Board();
-            
-            for (int i = 0; i < 12; i++)
+            board.GenerateCheckers();
+            board.DrawBoard();
+
+            Console.WriteLine("Did you want to 'move' or 'remove' a piece?");
+            string read = Console.ReadLine();
+
+            do
             {
-                Checker white = new Checker("white", whitePositions[i]);
-                Checker black = new Checker("black", blackPositions[i]);
-                board.Checkers.Add(white);
-                board.Checkers.Add(black);
-            }
-            board.DrawBoard();
-            Console.WriteLine("Select checker row:");
-            int row = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Select checker column:");
-            int col = Convert.ToInt32(Console.ReadLine());
+                switch (read)
+                {
+                    case "move":
+                        Console.WriteLine("Select checker row:");
+                        int row = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Select checker column:");
+                        int col = Convert.ToInt32(Console.ReadLine());
+                        
+                        Checker checker = board.SelectChecker(row, col);
+                        
+                        Console.WriteLine("Move to new row:");
+                        int newRow = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Move to new column:");
+                        int newCol = Convert.ToInt32(Console.ReadLine());
+                        checker.Position = new int[] { newRow, newCol };
+                        board.DrawBoard();
+                        continue;
+                    case "remove":
+                        Console.WriteLine("Select the row of the checker to remove:");
+                        int removeRow = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Select the column of the checker to remove:");
+                        int removeCol = Convert.ToInt32(Console.ReadLine());
+                        Checker doneChecker = board.SelectChecker(removeRow, removeCol);
+                        board.RemoveChecker(doneChecker);
+                        board.DrawBoard();
+                        continue;
+                    default:
+                        Console.WriteLine("I didn't get that.. what was that again?");
+                        continue;
+                }
+            } while (board.CheckForWin() == false);
 
-            Checker checker = board.SelectChecker(row, col);
-            Console.WriteLine("Move to new row:");
-            int newRow = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Move to new column:");
-            int newCol = Convert.ToInt32(Console.ReadLine());
-
-            checker.Position = new int[] { newRow, newCol };
-            board.DrawBoard();
-            Console.WriteLine("hello, world");
+            // Below is for debugging
+            // Console.WriteLine("hello, world");
         }
     }
 
@@ -100,7 +106,25 @@ namespace Checkers
         
         public void GenerateCheckers()
         {
-            // Your code here
+            int[][] whitePositions = new int[][] {
+                new int[] { 0, 1 }, new int[] { 0, 3 }, new int[] { 0, 5 }, new int[] { 0, 7 },
+                new int[] { 1, 0 }, new int[] { 1, 2 }, new int[] { 1, 4 }, new int[] { 1, 6 },
+                new int[] { 2, 1 }, new int[] { 2, 3 }, new int[] { 2, 5 }, new int[] { 2, 7 }
+            };
+            
+
+            int[][] blackPositions = new int[][] {
+                new int[] { 5, 0 }, new int[] { 5, 2 }, new int[] { 5, 4 }, new int[] { 5, 6 },
+                new int[] { 6, 1 }, new int[] { 6, 3 }, new int[] { 6, 5 }, new int[] { 6, 7 },
+                new int[] { 7, 0 }, new int[] { 7, 2 }, new int[] { 7, 4 }, new int[] { 7, 6 }
+            };
+            for (int i = 0; i < 12; i++)
+            {
+                Checker white = new Checker("white", whitePositions[i]);
+                Checker black = new Checker("black", blackPositions[i]);
+                Checkers.Add(white);
+                Checkers.Add(black);
+            }
             return;
         }
         
@@ -108,7 +132,7 @@ namespace Checkers
         {
             foreach (var checker in Checkers)
             {
-                Console.WriteLine(checker.Position[0] + " " + checker.Position[1]);
+                // Console.WriteLine(checker.Position[0] + " " + checker.Position[1]);
                 this.Grid[checker.Position[0]][checker.Position[1]] = checker.Symbol;
             }
             return;
@@ -133,7 +157,8 @@ namespace Checkers
         
         public void RemoveChecker(Checker checker)
         {
-            this.Checkers.Remove(checker);
+            // Checkers.Find(x => x.Position.SequenceEqual(new List<int> { killRow, killCol }));
+            Checkers.Remove(checker);
             return;
         }
         
